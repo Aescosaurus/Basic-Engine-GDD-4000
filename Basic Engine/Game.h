655497a4fd79f5f0aea3cddd309c4d4c1189fb6d
Aescosaurus <michaelbuslik@hotmail.com>
2020-10-10 12:01:17 -0600

    Game working again.

--- Basic Engine/Game.h
+++ Basic Engine/Game.h
@@ -6,6 +6,7 @@
 #include "GameWorld.h"
 #include "Vec2.h"
 #include <vector>
+#include "Timer.h"
 
 class Game
 {
@@ -26,4 +27,11 @@ private:
 
 	Vec2 playerPos;
 	std::vector<Vec2> bullets;
+	std::vector<Vec2> aliens;
+	Timer alienAnim = 0.5f;
+	char alienFrame = 'M';
+	int alienDir = 1;
+	int alienSpeed = 1;
+	Timer alienMove = 1.0f;
+	bool gameOver = false;
 };
